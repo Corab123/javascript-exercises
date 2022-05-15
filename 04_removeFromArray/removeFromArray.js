dc237fb6cdef19530b7395d9e91299677d07d9f1
Ákos Balogh <akos.balogh87@gmail.com>
2022-05-15 23:00:10 +0200

    Complete exercise 4."

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let args = Array.from(arguments);
+    let newArray=[];
+    for (item of array){
+        if(!args.includes(item)){
+            newArray.push(item);
+        }
+    }
+    return newArray;
+    
 };
 
 // Do not edit below this line
