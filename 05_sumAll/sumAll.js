ae1e320d5f00da2b64f057f8e62d1a8dbd93e7b3
Ákos Balogh <akos.balogh87@gmail.com>
2022-05-15 23:13:52 +0200

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a<0 || b<0 || !Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR"
+    }
+    if (a>b){
+        b = [a, a=b][0];
+    }
+    for (a; a<=b; a++){
+        sum += a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
